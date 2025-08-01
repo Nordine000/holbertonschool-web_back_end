60773b10981d3f1032b5a698674e78b164bd92f4
Nordine000 <ounissi970@gmail.com>
2025-08-01 21:04:16 +0200

    test

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Coroutine with async """
+
+from typing import AsyncGenerator
+import asyncio
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Loops 10 times asynchronously, yields random float each time."""
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
