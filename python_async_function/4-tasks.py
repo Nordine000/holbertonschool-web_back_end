9006cff96bdc88705c11b896681d920c111653fc
Nordine000 <ounissi970@gmail.com>
2025-08-01 20:55:55 +0200

    test

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 """
-Take the code from wait_n and alter it into a new function
-task_wait_n. The code is nearly identical to wait_n except
-task_wait_random is being called.
+Module contenant la fonction task_wait_n qui utilise des coroutines asynchrones.
 """
+
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
-
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Let's execute multiple coroutines at the same time with async
-    """
-    delays = []
-    coroutines = []
-    for number_coroutines in range(n):
-        coroutines.append(task_wait_random(max_delay))
+    Lance n tâches asynchrones et retourne la liste des délais dans l'ordre croissant.
 
-    for coroutine in asyncio.as_completed(coroutines):
-        # as_completed order coroutines by end of excecution (fastest first)
-        delay = await coroutine
-        delays.append(delay)
+    Args:
+        n (int): Nombre de fois que la tâche est lancée.
+        max_delay (int): Délai maximum pour chaque tâche.
+
+    Returns:
+        List[float]: Liste des délais d'attente, triés.
+    """
+    delays = [task_wait_random(max_delay) for _ in range(n)]
+    return sorted(await asyncio.gather(*delays))
