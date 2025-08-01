c3a95dfcc640fc2f6e46b38678e043afddd11e54
Nordine000 <ounissi970@gmail.com>
2025-08-01 20:57:44 +0200

    test

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 """
-Module contenant la fonction task_wait_n qui utilise des coroutines asynchrones.
+Module contenant la fonction task_wait_n qui utilise des
+coroutines asynchrones.
 """
 
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Lance n tâches asynchrones et retourne la liste des délais dans l'ordre croissant.
+    Lance n tâches asynchrones et retourne la
+    liste des délais dans l'ordre croissant.
 
     Args:
         n (int): Nombre de fois que la tâche est lancée.
