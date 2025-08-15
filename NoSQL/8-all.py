5d8b23bee4c40f5079a72050f9ed5aa52008d4a6
Nordine000 <ounissi970@gmail.com>
2025-08-15 02:37:54 +0200

    test

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,6 +5,7 @@ import pymongo
 
 
 def list_all(mongo_collection):
+    """ List all elements in a collection """
     if not mongo_collection:
         return []
     return list(mongo_collection.find())
