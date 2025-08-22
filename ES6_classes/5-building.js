9f465c511ec4fd7f6030082a37d28e0d6ad702af
Nordine000 <ounissi970@gmail.com>
2025-08-22 16:38:19 +0200

    test

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,16 @@
 export default class Building {
-  constructor(sqft) {
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-        throw Error('Class extending Building must override evacuationWarningMessage');
+    constructor(sqft) {
+        if (this.constructor !== Building && !this.evacuationWarningMessage) {
+            throw Error('Class extending Building must override evacuationWarningMessage');
+        }
+        if (typeof sqft !== 'number') {
+            throw new TypeError('Sqft must be a number');
+        }
+        this._sqft = sqft;
     }
-    if (typeof sqft !== 'number') {
-        throw new TypeError('Sqft must be a number');
-    }
-    this._sqft = sqft;
-}
     get sqft() {
-    return this._sqft;
-}
+        return this._sqft;
+    }
     set sqft(newsqft) {
         if (typeof newsqft !== 'number') {
             throw new TypeError('Sqft must be a number');
