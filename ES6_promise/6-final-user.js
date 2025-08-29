0619510cb6efd21de2c02f69c583eccb73c98f0b
Nordine000 <ounissi970@gmail.com>
2025-08-29 21:29:09 +0200

    test

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,22 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const userPromise = signUpUser(firstName, lastName);
-    const photoPromise = uploadPhoto(fileName);
+    const signUpPromise = signUpUser(firstName, lastName);
+    const uploadPromise = uploadPhoto(fileName);
     
-    return Promise.allSettled([userPromise, photoPromise]).then((results) =>
-        results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : String(result.reason),
-        }))
+    return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) =>
+        results.map((result) => {
+            if (result.status === 'fulfilled') {
+                return {
+                    status: result.status,
+                    value: result.value,
+                };
+            }
+            return {
+                status: result.status,
+                value: result.reason,
+            };
+        })
     );
 }
