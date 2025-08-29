69cf50aa5aa11e43fd6b1dbd8a99d6d99ddbd800
Nordine000 <ounissi970@gmail.com>
2025-08-29 21:26:24 +0200

    test

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-    const userData = await signUpUser(firstName, lastName).then((data) => ({
-        status: 'fulfilled',
-        value: data,
-    }));
-
-    const fileData = await uploadPhoto(fileName).catch((err) => ({
-        status: 'rejected',
-        value: err.toString(),
-    }));
-
-    return Promise.resolve([userData, fileData]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+    
+    return Promise.allSettled([userPromise, photoPromise]).then((results) =>
+        results.map((result) => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : String(result.reason),
+        }))
+    );
 }
